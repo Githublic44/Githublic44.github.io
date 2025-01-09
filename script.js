7eced286b8e7711f68772c3ca7e5264956daede7
BiTZC <131499941+Githublic44@users.noreply.github.com>
2025-01-09 14:31:02 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-let num = 50;
-if (num%2=='0'){
-    console.log(num);
+function openwindow(site)  {
+    switch(site){
+        case 'discord':
+            window.open('https://discord.gg/38k5DnnU');
+            break;
+        case 'scratch':
+            window.open('https://scratch.mit.edu/users/BiTZC/');
+            break;
+        case 'github':
+            window.open('https://github.com/Githublic44/Githublic44.github.io');
+            break;
+    }
 }
